51e2df6ea42565cad6d2a13eea278c664a9835ad
deepak-thakur <deepak.thakur@inmobi.com>
2017-08-28 11:40:47 +0530

    complete schema update

--- sql/schema/comment.sql
+++ sql/schema/comment.sql
@@ -6,9 +6,11 @@ CREATE TABLE comment (
     text VARCHAR,
     image_id VARCHAR,
     question_id INT NOT NULL,
+    user_id INT NOT NULL,
     CONSTRAINT comment_pk PRIMARY KEY(comment_id),
-    CONSTRAINT comment_question_fk FOREIGN KEY(question_id) REFERENCES question(question_id) ON DELETE CASCADE
+    CONSTRAINT comment_user_id_fk FOREIGN KEY(user_id) REFERENCES user(user_id),
+    CONSTRAINT comment_question_id_fk FOREIGN KEY(question_id) REFERENCES question(question_id) ON DELETE CASCADE
 );
 CREATE INDEX comment_question_pk_idx on comment USING hash(comment_id, question_id);
-CREATE INDEX comment_id_idx on comment USING hash(comment_id);
-CREATE INDEX question_id_idx on comment USING hash(question_id);
+CREATE INDEX comment_comment_id_idx on comment USING hash(comment_id);
+CREATE INDEX comment_question_id_idx on comment USING hash(question_id);
